43d0ead2ebd58b0f27c19dc97b0bfea7fb8efc7b
Oisin Cleary <oisinc@Oisins-MacBook-Air.local>
2025-12-04 05:22:51 +0000

    2025 day4

---
+++ 2025/4/part2.py
@@ -0,0 +1,36 @@
+def getResult( lines ):
+	total, prev = 0, -1
+	height = len(lines)
+	width = len(lines[0])
+	while (total != prev):
+		prev = total
+		for i in range(height):
+			for j in range(width):
+				if lines[i][j] != '@':
+					continue
+				count = 0
+				for xOffset in range(3):
+					for yOffset in range(3):
+						x = i+xOffset-1
+						y = j+yOffset-1
+						if x >= 0 and y >= 0 and x < height and y < width and lines[x][y] == '@':
+							count += 1
+				if count < 5:
+					lines[i] = lines[i][:j] + 'x' + lines[i][j+1:]
+					total += 1
+					continue
+	return total
+
+def readData( filename ):
+	return [ x for x in open( filename ).read().strip().split( '\n' ) ]
+
+def runTest( testData, expected ):
+	result = getResult( testData )
+	if expected == result:
+		return True
+	print(f"Test failed, expcted {expected} got {result}")
+	return False
+
+if runTest( readData( "input.tst" ), 43 ):
+	print( getResult( readData( "input.txt" ) ) )
+	print( "Done" )
